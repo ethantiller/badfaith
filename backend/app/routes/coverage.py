8f6851005e533501adde7a414d2b33ac6a356a44
Ethan Tiller <tilleres@mail.uc.edu>
2026-09-19 16:09:43 -0400

    boom

--- backend/app/routes/coverage.py
+++ backend/app/routes/coverage.py
@@ -4,15 +4,9 @@ from ddgs.exceptions import DDGSException
 from fastapi import APIRouter, HTTPException
 
 from backend.app.lib.helpers.web_search import search
-from backend.app.lib import types
+from backend.app.lib.types import CoverageRequest, CoverageResponse, RelatedSource, Omission, CoverageMeta
 
 router = APIRouter()
-
-CoverageRequest = types.CoverageRequest
-CoverageResponse = types.CoverageResponse
-RelatedSource = types.RelatedSource
-Omission = types.Omission
-CoverageMeta = types.CoverageMeta
 
 
 @router.post(
